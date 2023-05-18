d093fd270f2b277f9c8ea2dbe9ab1adbe87f4a56
akashi7 <christiannseko@gmail.com>
2023-05-18 17:20:46 +0200

    tailwind media query

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -22,9 +22,9 @@ const videos =
 const HomePage: FC = (): ReactElement => {
   return (
     <Layout className='w-full h-[100%] bg-white'>
-      <div className='p-[8px] lg:p-[0px] '>
+      <div className='p-[8px] lg:p-[0px] xl:p-[30px] '>
         <div className='flex flex-col lg:mt-[-50px] mt-[60px]  w-full lg:h-[720px] lg:flex-row items-center lg:w-[80%] lg:mx-auto  lg:p-[0px] '>
-          <div className=' w-full lg:mt-[0px] mt-[15px] md:w-[80%] lg:w-[57%]  md:pl-[0px] xl:pl-[250px]  md:text-center xl:text-left'>
+          <div className=' w-full lg:mt-[0px] mt-[15px] md:w-[80%] lg:w-[100%]  xl:w-[60%]  md:pl-[0px]   md:text-center xl:text-left'>
             <div className='lg:mb-[15px] w-full'>
               <p className='font-bold lg:text-3xl text-black md:text-2xl text-base w-full'>
                 hello, ar과 함께 하는
@@ -42,14 +42,14 @@ const HomePage: FC = (): ReactElement => {
               </p>
             </div>
           </div>
-          <div className='w-full md:pl-[0px] xl:pl-[10px] p-[10px] lg:p-[0px]'>
+          <div className='w-full md:pl-[0px] p-[10px] lg:p-[0px] lg:flex lg:justify-end'>
             <img src={mockup} alt='mockup' className=' md:w-[100%]  w-[100%]' />
           </div>
         </div>
         <div className='md:mb-[50px] xl:mb-[136px]'></div>
         <div className='flex  flex-col-reverse  mt-[15px] lg:flex-row  items-center w-full lg:w-[80%] lg:mx-auto  lg:p-[0px]'>
-          <div className='w-[100%]  md:pl-[0px] xl:pl-[250px] md:text-center xl:text-left'>
-            <div className='lg:mb-[10px] mt-[10px]'>
+          <div className='w-[100%]  md:pl-[0px] lg:text-left  md:text-center xl:text-left'>
+            <div className='lg:mb-[10px] mt-[10px] xl:mt-[0px]'>
               <p className='text-hblue'>손쉬운 파일 등록</p>
             </div>
             <div className='lg:mb-[15px] mt-[10px]'>
@@ -69,7 +69,7 @@ const HomePage: FC = (): ReactElement => {
               </p>
             </div>
           </div>
-          <div className='w-[70%] xl:w-[100%] md:w-[60%]  md:pl-[0px] xl:pl-[100px] mb-[30px] mt-[20px] lg:mt-[0px]  lg:mb-[0px]'>
+          <div className='w-[70%] xl:w-[100%] md:w-[60%] lg:flex lg:justify-end md:pl-[0px]  mb-[30px] mt-[20px] lg:mt-[0px]  lg:mb-[0px]'>
             <img
               src={imageOne}
               alt='imageOne'
@@ -79,53 +79,53 @@ const HomePage: FC = (): ReactElement => {
         </div>
         <div className='md:mb-[120px] xl:mb-[236px] mt-[50px] lg:mt-[0px]'></div>
         <div className='flex  flex-col w-full  lg:flex-row items-center lg:w-[80%] lg:mx-auto  lg:p-[0px] '>
-          <div className='md:w-[50%] xl:w-[65%] w-[100%] xl:pl-[250px] mb-[50px] lg:mb-[0px]'>
+          <div className='md:w-[50%] xl:w-[65%] w-[100%]  mb-[50px] lg:mb-[0px]'>
             <div className='flex justify-between '>
-              <div className='bg-hgrey w-full m-[5px] lg:m-[10px] rounded-lg'>
+              <div className='bg-hgrey w-full m-[5px]  xl:m-[5px] lg:m-[10px] rounded-lg'>
                 <img src={blue} alt='blue' />
               </div>
-              <div className='bg-hgrblue  w-full m-[5px] lg:m-[10px] rounded-lg'>
+              <div className='bg-hgrblue  w-full m-[5px] xl:m-[5px] lg:m-[10px] rounded-lg'>
                 <img src={green} alt='green' />
               </div>
             </div>
-            <div>
-              <div className='flex justify-between '>
-                <div className='bg-hgreen  m-[5px] lg:m-[10px] rounded-lg'>
-                  <img src={pink} alt='pink' />
-                </div>
-                <div className='bg-hgray  m-[5px] lg:m-[10px] rounded-lg'>
-                  <img src={white} alt='white' />
-                </div>
+            <div className='flex justify-between '>
+              <div className='bg-hgreen xl:w-full m-[5px] xl:m-[5px] lg:m-[10px] rounded-lg'>
+                <img src={pink} alt='pink' />
+              </div>
+              <div className='bg-hgray xl:w-full m-[5px] xl:m-[5px] lg:m-[10px] rounded-lg'>
+                <img src={white} alt='white' />
               </div>
             </div>
           </div>
-          <div className='w-full  mt-[10px] lg:mt-[0px] md:pl-[0px] md:text-center  xl:text-left  xl:pl-[300px]'>
-            <div className='lg:mb-[10px] mb-[10px]'>
-              <p className='text-sm text-hpurple break-all'>
-                실제와 가까운 제품 확인을 위해
-              </p>
-            </div>
-            <div className='lg:mb-[15px] mt-[10px] lg:mt-[0px]'>
-              <p className='font-bold lg:text-3xl text-xl break-all text-black'>
-                하나의 제품이라도 다른
-              </p>
-              <p className='font-bold lg:text-3xl text-xl break-all text-black'>
-                사이즈, 칼라, 소재를 모두 담아
-              </p>
-            </div>
-            <div className='mt-[10px] mb-[10px] lg:mt-[0px] lg:mb-[0px]'>
-              <p className='font-medium lg:text-sm break-all text-Hblack'>
-                사이즈, 칼라, 소재까지 모두 반영해서 쇼핑몰 고객들이 실제와
-              </p>
-              <p className='font-medium lg:text-sm break-all text-Hblack'>
-                차이 없는 경험을 할 수 있습니다.
-              </p>
+          <div className='w-full  mt-[10px] lg:mt-[0px] md:pl-[0px] md:text-center lg:text-left  lg:flex lg:justify-end xl:justify-center  xl:text-left  xl:pl-[150px]'>
+            <div>
+              <div className='lg:mb-[10px] mb-[10px]'>
+                <p className='text-sm text-hpurple break-all'>
+                  실제와 가까운 제품 확인을 위해
+                </p>
+              </div>
+              <div className='lg:mb-[15px] mt-[10px] lg:mt-[0px]'>
+                <p className='font-bold lg:text-3xl text-xl break-all text-black'>
+                  하나의 제품이라도 다른
+                </p>
+                <p className='font-bold lg:text-3xl text-xl break-all text-black'>
+                  사이즈, 칼라, 소재를 모두 담아
+                </p>
+              </div>
+              <div className='mt-[10px] mb-[10px] lg:mt-[0px] lg:mb-[0px]'>
+                <p className='font-medium lg:text-sm break-all text-Hblack'>
+                  사이즈, 칼라, 소재까지 모두 반영해서 쇼핑몰 고객들이 실제와
+                </p>
+                <p className='font-medium lg:text-sm break-all text-Hblack'>
+                  차이 없는 경험을 할 수 있습니다.
+                </p>
+              </div>
             </div>
           </div>
         </div>
-        <div className='xl:mb-[200px]'></div>
+        <div className='xl:mb-[200px] lg:mb-[200px]'></div>
         <div className='flex  flex-col-reverse lg:flex-row items-center lg:w-[80%] lg:mx-auto  lg:p-[0px] '>
-          <div className='w-[100%] xl:pl-[250px] xl:text-left  mt-[20px] md:text-center '>
+          <div className='w-[100%]  xl:text-left  mt-[20px] md:text-center lg:text-left '>
             <div className='lg:mb-[10px] mt-[10px] lg:mt-[0px] '>
               <p className='text-sm text-htgreen'>AR 서비스 애널리틱스</p>
             </div>
@@ -144,11 +144,11 @@ const HomePage: FC = (): ReactElement => {
               </p>
             </div>
           </div>
-          <div className='w-[50%] xl:w-full md:w-full xl:pl-[100px]  md:flex md:justify-center xl:justify-start  md:text-center xl:text-left  md:pl-[0px] mt-[50px] lg:mt-[0px]'>
+          <div className='w-[50%] xl:w-full md:w-full   md:flex md:justify-center xl:justify-center lg:justify-center md:text-center xl:text-left  md:pl-[0px] mt-[50px] lg:mt-[0px]'>
             <img
               src={imagethree}
               alt='imagethre'
-              className=' md:w-[40%] xl:w-[50%] w-full '
+              className=' md:w-[40%] xl:w-[50%] lg:w-[50%] w-full '
             />
           </div>
         </div>
@@ -160,17 +160,19 @@ const HomePage: FC = (): ReactElement => {
         </div>
         <div className='lg:mb-[78px]'></div>
         <div className='flex  flex-col lg:flex-row lg:items-center lg:w-[80%] w-full lg:mx-auto  lg:p-[0px]'>
-          <div className='w-[100%]  flex flex-col items-center '>
-            <div className='md:pl-[0px] mb-[60px] lg:mb-[0px] xl:pl-[200px] '>
-              <div className='flex justify-center bg-hroundy rounded-full mx-auto xl:w-[260px] md:w-[290px] w-[66%] xl:h-[240px] h-fit lg:p-[65px] p-[45px] '>
-                <img
-                  src={salyeight}
-                  alt='salyeight'
-                  className='object-cover object-center w-full lg:w-full '
-                />
+          <div className='w-[100%]  flex flex-col  '>
+            <div className='md:pl-[0px] mb-[60px] lg:mb-[0px] xl:flex xl:justify-center '>
+              <div>
+                <div className='flex justify-center bg-hroundy rounded-full mx-auto xl:w-[260px] md:w-[290px] w-[66%] xl:h-[240px] h-fit lg:p-[65px] p-[45px] '>
+                  <img
+                    src={salyeight}
+                    alt='salyeight'
+                    className='object-cover object-center w-full lg:w-full '
+                  />
+                </div>
               </div>
             </div>
-            <div className='w-full md:pl-[0px] xl:pl-[250px] lg:mt-[40px] md:text-center xl:text-left'>
+            <div className='w-full md:pl-[0px] lg:mt-[40px] md:text-center  xl:text-center'>
               <div className='lg:mb-[15px]'>
                 <p className='font-bold lg:text-2xl text-black break-all'>
                   복잡한 등록 과정이나 절차 없이 손쉬운 제품
@@ -190,17 +192,19 @@ const HomePage: FC = (): ReactElement => {
               </div>
             </div>
           </div>
-          <div className='w-[100%] flex flex-col items-center mt-[100px] lg:mt-[0px] md:pl-[20px]'>
-            <div className=' md:pr-[0px] xl:pr-[330px] mb-[30px] lg:mb-[0px] '>
-              <div className='flex justify-center bg-hroundp rounded-full mx-auto xl:w-[260px] md:w-[290px] w-[80%] h-fit xl:h-[240px] lg:p-[45px] p-[45px]'>
-                <img
-                  src={salysix}
-                  alt='salysix'
-                  className='object-cover object-center w-[70%] '
-                />
+          <div className='w-[100%] flex flex-col   mt-[100px] lg:mt-[0px] md:pl-[20px] xl:pl-[0px]'>
+            <div className=' md:pr-[0px] mb-[30px] lg:mb-[0px] xl:flex xl:justify-center'>
+              <div>
+                <div className='flex justify-center bg-hroundp rounded-full mx-auto xl:w-[260px] md:w-[290px] w-[80%] h-fit xl:h-[240px] lg:p-[45px] p-[45px]'>
+                  <img
+                    src={salysix}
+                    alt='salysix'
+                    className='object-cover object-center w-[70%] '
+                  />
+                </div>
               </div>
             </div>
-            <div className='w-full lg:pl-[0px] lg:mt-[40px] md:text-center xl:text-left '>
+            <div className='w-full lg:pl-[0px] lg:mt-[40px] md:text-center xl:text-center  '>
               <div className='lg:mb-[15px]'>
                 <p className='font-bold lg:text-2xl text-black text-base break-all'>
                   AR뿐만 아니라 쇼핑몰 운영에 필요한 모든
@@ -222,19 +226,19 @@ const HomePage: FC = (): ReactElement => {
           </div>
         </div>
         <div className='md:mb-[107px] xl:mb-[237px] mt-[80px] lg:mt-[0px]'></div>
-        <div className='lg:w-[80%] w-full lg:mx-auto'>
-          <div className='md:w-[100%] xl:w-[90%] xl:pl-[250px]  w-full md:pl-[0px] border-solid border-1 border-black'>
+        <div className='lg:w-[80%] xl:w-[80%]  w-full lg:mx-auto'>
+          <div className='md:w-[100%] xl:w-[100%]  w-full md:pl-[0px]'>
             <video src={videos} controls></video>
           </div>
         </div>
         <div className='md:mb-[20px] xl:mb-[20px] mb-[40px]'></div>
         <div className='lg:w-[80%] w-full lg:mx-auto  lg:p-[0px]'>
-          <div className='lg:w-[50%] w-full md:pl-[0px] xl:pl-[250px] mb-[20px]'>
+          <div className='lg:w-[50%] w-full md:pl-[0px]  mb-[20px]'>
             <p className='font-bold lg:text-3xl text-xl  text-black'>
               AR 서비스 관리는 이렇게 이용해 보세요!
             </p>
           </div>
-          <div className='w-fit md:pl-[0px] xl:pl-[250px]'>
+          <div className='w-fit md:pl-[0px] '>
             <p className='font-medium lg:text-sm text-xs break-all text-Hblack'>
               ·원하는 제품만 골라서 라이브시킬 수 있고 나머지 제품들은 대기중인
               목록에서 관리할 수 있습니다
@@ -246,18 +250,18 @@ const HomePage: FC = (): ReactElement => {
         </div>
         <div className='lg:mb-[157px] mb-[80px] '></div>
         <div className='lg:w-[80%] lg:mx-auto'>
-          <div className='md:w-[100%] xl:w-[90%] xl:pl-[250px] w-full md:pl-[0px] border-solid border-1 border-black '>
+          <div className='md:w-[100%] xl:w-[100%]  w-full md:pl-[0px] border-solid border-1 border-black '>
             <video src={videos} controls></video>
           </div>
         </div>
         <div className='lg:mb-[20px] mb-[40px] '></div>
         <div className='lg:w-[80%] w-full lg:mx-auto  lg:p-[0px]'>
-          <div className='lg:w-[50%] w-full md:pl-[0px] xl:pl-[250px] lg:mb-[20px]  '>
+          <div className='lg:w-[50%] w-full md:pl-[0px]  lg:mb-[20px]  '>
             <p className='font-bold lg:text-3xl text-xl  text-black break-all'>
               AR 서비스 관리는 이렇게 이용해 보세요!
             </p>
           </div>
-          <div className='w-fit md:pl-[0px] xl:pl-[250px] '>
+          <div className='w-fit md:pl-[0px]  '>
             <p className='font-medium lg:text-sm text-Hblack break-all'>
               · 몇 명의 사람들이 얼마나 고객님의 몰에 머물렀는지 분석할 수
               있습니다.
@@ -280,7 +284,7 @@ const HomePage: FC = (): ReactElement => {
         </div>
         <div className='lg:mb-[50px] mb-[40px]  '></div>
         <div className='md:w-[100%] xl:w-[80%] w-full lg:mx-auto  lg:p-[0px] '>
-          <div className='md:pl-[0px] xl:pl-[180px] '>
+          <div className='md:pl-[0px] '>
             <div className=' w-full flex  md:justify-evenly flex-wrap md:flex-nowrap  items-center '>
               <div className=' w-[50%] '>
                 <img src={godbrave} alt='godbrave' className=' md:w-[60%]' />
