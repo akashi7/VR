4c5ae182eac2d476fbd79dab17aa88feff9ad72a
akashi7 <christiannseko@gmail.com>
2023-09-14 10:15:20 +0200

    changed position of uv data

--- src/components/dashboard/common/products/uv.cards.tsx
+++ src/components/dashboard/common/products/uv.cards.tsx
@@ -7,17 +7,17 @@ interface analyticsI {
 
 const UvCards: FC<analyticsI> = ({ analytics }): ReactElement => {
   return (
-    <div className='flex   justify-between w-[50%] '>
-      <div>
-        <div className='font-bold  text-black'>구매자 수</div>
-        <div className='text-[#4270ED] font-bold   text-lg'>
-          {analytics?.total_views || 0}
-        </div>
-      </div>
+    <div className='flex  justify-between w-[50%] '>
       <div>
         <div className='font-bold  text-black'>방문자 수</div>
         <div className='text-[#4270ED] font-bold   text-lg'>
           {analytics?.total_purchases || 0}
+        </div>
+      </div>
+      <div>
+        <div className='font-bold  text-black'>구매자 수</div>
+        <div className='text-[#4270ED] font-bold   text-lg'>
+          {analytics?.total_views || 0}
         </div>
       </div>
       <div>
