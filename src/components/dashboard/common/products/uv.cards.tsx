cfde1bf1c8d6f07e50fd4ed3b249202ebb9bf82f
akashi7 <christiannseko@gmail.com>
2023-09-18 10:33:50 +0200

    switch views position

--- src/components/dashboard/common/products/uv.cards.tsx
+++ src/components/dashboard/common/products/uv.cards.tsx
@@ -9,20 +9,20 @@ const UvCards: FC<analyticsI> = ({ analytics }): ReactElement => {
   return (
     <div className='flex  justify-between w-[50%] '>
       <div>
-        <div className='font-bold  text-black'>방문자 수</div>
-        <div className='text-[#4270ED] font-bold   text-lg'>
-          {analytics?.total_purchases || 0}
-        </div>
-      </div>
-      <div>
         <div className='font-bold  text-black'>구매자 수</div>
-        <div className='text-[#4270ED] font-bold   text-lg'>
+        <div className='text-[#4270ED] font-bold text-lg'>
           {analytics?.total_views || 0}
         </div>
       </div>
       <div>
+        <div className='font-bold  text-black'>방문자 수</div>
+        <div className='text-[#4270ED] font-bold text-lg'>
+          {analytics?.total_purchases || 0}
+        </div>
+      </div>
+      <div>
         <div className='font-bold  text-black'>구매전환율 (%) </div>
-        <div className='text-[#4270ED] font-bold   text-lg'>
+        <div className='text-[#4270ED] font-bold  text-lg'>
           {(analytics?.total_rate ?? 0).toFixed(2) || 0} {'%'}
         </div>
       </div>
